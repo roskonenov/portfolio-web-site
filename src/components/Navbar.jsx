9ab666509f5e636c3e2533827d790990fb8364f1
roskonenov <roskonenov@gmail.com>
2025-07-01 21:17:14 +0300

    add NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,45 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
-            <img src={Logo} alt="Logo image" style={{height: '50px'}}/>
-        </div>
-        <div>
-            <ul className='flex'>
+    const [ nav, setNav ] = useState(false);
+    const handleClick = () => setNav(!nav);
+    
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
+                <img src={Logo} alt="Logo image" style={{ height: '50px' }} />
+            </div>
+
+            {/* Menu */}
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
                 <li>Work</li>
                 <li>Contact</li>
             </ul>
-        </div>
-        <div className='hidden'>
-            <FaBars/>
-        </div>
-        <div>
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+
+            {/* Hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+
+            {/* Mobile Menu */}
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
+
+            {/* Social Icons */}
+            <div className='hidden'>
+            </div>
         </div>
-        <div></div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
