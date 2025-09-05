d333f63459f29ad30998dfc62009d80e982d9fd1
roskonenov <roskonenov@gmail.com>
2025-09-05 23:22:56 +0300

    fix for Skills icons not showing

--- src/data/Data.js
+++ src/data/Data.js
@@ -2,61 +2,61 @@ export const skillCardsData = [
     {
         id: 1,
         name: 'JAVA',
-        icon: 'src/assets/java.png'
+        icon: '/src/assets/java.png'
     },
     {
         id: 2,
         name: 'SPRING',
-        icon: 'src/assets/spring.png'
+        icon: '/src/assets/spring.png'
     },
     {
         id: 3,
         name: 'MYSQL',
-        icon: 'src/assets/mysql.png'
+        icon: '/src/assets/mysql.png'
     },
     {
         id: 4,
         name: 'THYMELEAF',
-        icon: 'src/assets/thymeleaf.png'
+        icon: '/src/assets/thymeleaf.png'
     },
     {
         id: 5,
         name: 'HTML',
-        icon: 'src/assets/html.png'
+        icon: '/src/assets/html.png'
     },
     {
         id: 6,
         name: 'CSS',
-        icon: 'src/assets/css.png'
+        icon: '/src/assets/css.png'
     },
     {
         id: 7,
         name: 'JAVASCRIPT',
-        icon: 'src/assets/javascript.png'
+        icon: '/src/assets/javascript.png'
     },
     {
         id: 8,
         name: 'REACT',
-        icon: 'src/assets/react.png'
+        icon: '/src/assets/react.png'
     },
     {
         id: 9,
         name: 'TAILWIND',
-        icon: 'src/assets/tailwind.png'
+        icon: '/src/assets/tailwind.png'
     },
     {
         id: 10,
         name: 'GITHUB',
-        icon: 'src/assets/github.png'
+        icon: '/src/assets/github.png'
     },
     {
         id: 11,
         name: 'GCLOUD',
-        icon: 'src/assets/gcloud.png'
+        icon: '/src/assets/gcloud.png'
     },
     {
         id: 12,
         name: 'FIREBASE',
-        icon: 'src/assets/firebase.png'
+        icon: '/src/assets/firebase.png'
     },
 ];
