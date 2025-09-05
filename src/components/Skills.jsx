0a2b0a6c12d5800a05ac2f8d64a6842f3aca0e88
roskonenov <roskonenov@gmail.com>
2025-09-05 21:14:44 +0300

    add SkillCard component with more styling based on react-parallax-tilt, add data file

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import Java from '../assets/java.png';
-import Spring from '../assets/spring.png';
-import GCloud from '../assets/gcloud.png';
-import Firebase from '../assets/firebase.png';
-import CSS from '../assets/css.png';
-import HTML from '../assets/html.png';
-import JavaScript from '../assets/javascript.png';
-import ReactLogo from '../assets/react.png';
-import TailWind from '../assets/tailwind.png';
-import MySql from '../assets/mysql.png';
-import Thymeleaf from '../assets/thymeleaf.png';
-import GitHub from '../assets/github.png';
+import { skillCardsData } from '../data/Data.js'
+import SkillCard from './SkillCard.jsx';
 
 const Skills = () => {
     return (
@@ -21,54 +11,10 @@ const Skills = () => {
                     <p className='py-4'>This are the technologies I've experience with</p>
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={Java} alt="Java logo" />
-                        <p className='my-4'>JAVA</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={Spring} alt="Spring logo" />
-                        <p className='my-4'>SPRING</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={MySql} alt="MySql logo" />
-                        <p className='my-4'>MYSQL</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={Thymeleaf} alt="Thymeleaf logo" />
-                        <p className='my-4'>THYMELEAF</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML logo" />
-                        <p className='my-4'>HTML</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={CSS} alt="CSS logo" />
-                        <p className='my-4'>CSS</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript logo" />
-                        <p className='my-4'>JAVASCRIPT</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={ReactLogo} alt="React logo" />
-                        <p className='my-4'>REACT</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={TailWind} alt="Tailwind logo" />
-                        <p className='my-4'>TAILWIND</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={GitHub} alt="Github logo" />
-                        <p className='my-4'>GITHUB</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={GCloud} alt="GCloud logo" />
-                        <p className='my-4'>GCLOUD</p>
-                    </div>
-                    <div className='shadow-xs shadow-[#fff3b0] hover:scale-110 duration-500 rounded-xs pt-2'>
-                        <img className='w-20 mx-auto' src={Firebase} alt="Firebase logo" />
-                        <p className='my-4'>FIREBASE</p>
-                    </div>
+                    {skillCardsData.map(skill =>
+                        <SkillCard key={skill.id}
+                            skillData={skill} />
+                    )}
                 </div>
             </div>
         </div>
---
+++ src/data/Data.js
@@ -0,0 +1,62 @@
+export const skillCardsData = [
+    {
+        id: 1,
+        name: 'JAVA',
+        icon: 'src/assets/java.png'
+    },
+    {
+        id: 2,
+        name: 'SPRING',
+        icon: 'src/assets/spring.png'
+    },
+    {
+        id: 3,
+        name: 'MYSQL',
+        icon: 'src/assets/mysql.png'
+    },
+    {
+        id: 4,
+        name: 'THYMELEAF',
+        icon: 'src/assets/thymeleaf.png'
+    },
+    {
+        id: 5,
+        name: 'HTML',
+        icon: 'src/assets/html.png'
+    },
+    {
+        id: 6,
+        name: 'CSS',
+        icon: 'src/assets/css.png'
+    },
+    {
+        id: 7,
+        name: 'JAVASCRIPT',
+        icon: 'src/assets/javascript.png'
+    },
+    {
+        id: 8,
+        name: 'REACT',
+        icon: 'src/assets/react.png'
+    },
+    {
+        id: 9,
+        name: 'TAILWIND',
+        icon: 'src/assets/tailwind.png'
+    },
+    {
+        id: 10,
+        name: 'GITHUB',
+        icon: 'src/assets/github.png'
+    },
+    {
+        id: 11,
+        name: 'GCLOUD',
+        icon: 'src/assets/gcloud.png'
+    },
+    {
+        id: 12,
+        name: 'FIREBASE',
+        icon: 'src/assets/firebase.png'
+    },
+];
