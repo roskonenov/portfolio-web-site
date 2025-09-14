3b048ae6a2fa8617f9729334c32fab267e8601c8
roskonenov <roskonenov@gmail.com>
2025-09-14 15:19:33 +0300

    add picture to the About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import graduationPicture from '../../assets/graduationPicture.jpg'
 
 const About = () => {
     return (
-        <div name='about' className='w-full h-screen bg-[#335c67] text-[#e09f3e]'>
+        <div name='about' className='w-full min-h-screen bg-[#335c67] text-[#e09f3e]'>
             <div className='flex flex-col justify-center items-center w-full h-full px-4 md:pl-16'>
                 <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                     <div className='pb-8'>
@@ -17,8 +17,10 @@ const About = () => {
                     <div className='text-bg text-4xl font-bold col-span-2'>
                         <p className=''>Hi, I'm Rosen, nice to meet you. Please take a look around.</p>
                     </div>
-                    <img className='col-span-2 sm:col-span-1' src={graduationPicture} alt="Graduation picture" />
-                    <p className='col-span-2 sm:col-span-1 text-sm sm:text-md md:text-lg'>
+                    <div className='h-fit bg-cover bg-center col-span-2 sm:col-span-1 border-3 border-[#9e2a2b] rounded-4xl overflow-hidden'>
+                        <img  src={graduationPicture} alt="Graduation picture" />
+                    </div>
+                    <p className='col-span-2 sm:col-span-1 text-sm md:text-[1rem] lg:text-lg'>
                         I am motivated and detail-oriented with a strong foundation in Java (Spring Boot) and modern front-end technologies like React and JavaScript. Experienced in building real-world applications, following clean code principles and best practices in both back-end and front-end development. Skilled in working with relational databases (MySQL) and designing RESTful APIs. Passionate about continuous learning and eager to contribute to innovative software projects in a collaborative team environment.
                     </p>
                 </div>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
         e.target.reset();
     }
     return (
-        <div name='contact' className='w-full h-screen lg:mt-0 mt-56 bg-[#335c67] flex justify-center items-center p-4 md:pl-16'>
+        <div name='contact' className='w-full h-screen lg:mt-0 mt-56 bg-[#335c67] flex justify-center items-center p-4'>
             <form ref={form} onSubmit={sendEmail} action="" className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-[#9e2a2b] text-[#fff3b0]'>Contact</p>
